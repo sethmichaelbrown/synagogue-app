7efe8c0de0853df131802135961c94d1574015bb
SMB <sethmichaelbrown@gmail.com>
2019-04-08 16:33:47 -0700

    Adding object creation on click

--- src/components/admin/AdminPortal.js
+++ src/components/admin/AdminPortal.js
@@ -5,11 +5,9 @@ class AdminPortal extends Component {
   state = {
     adminStatus: false,
     displayItem: '',
-    newEvent: {
-      title: '',
-      description: '',
-      date: ''
-    }
+    description: '',
+    title: '',
+    date: ''
   }
 
   display = (event) => {
@@ -24,20 +22,43 @@ class AdminPortal extends Component {
     const textArea = document.getElementById('textarea2')
   }
 
+  newText = (event) => {
+    this.setState({ ...this.state, description: event.target.value })
+  }
+
+  newTitle = (event) => {
+    this.setState({ ...this.state, title: event.target.value })
+  }
+
+  newDate = (event) => {
+    this.setState({ ...this.state, date: event.target.value })
+  }
+
+  createNewEvent = () => {
+    const newEvent = {
+      title: this.state.title,
+      description: this.state.description,
+      date: this.state.date
+    }
+
+    console.log(newEvent)
+
+  }
+
   render() {
     return (
       <div className="AdminPortal">
         <div className="row container">
 
           <div className="row">
-            <a onClick={this.props.toAdmin} class="waves-effect waves-light btn">Home</a>
+            <a onClick={this.props.toAdmin} className="waves-effect waves-light btn">Home</a>
           </div>
 
 
           <div className="row">
-            <a onClick={this.display} id='create' class="waves-effect waves-light btn">Create Event</a>
-            <a onClick={this.display} id='edit' class="waves-effect waves-light btn">Edit Event</a>
-            <a onClick={this.display} id='search' class="waves-effect waves-light btn">Search Event</a>
+            <a onClick={this.display} id='create' className="waves-effect waves-light btn">Create Event</a>
+            <a onClick={this.display} id='edit' className="waves-effect waves-light btn">Edit Event</a>
+            <a onClick={this.display} id='search' className="waves-effect waves-light btn">Search Event</a>
           </div>
 
           <div className="row">
@@ -45,18 +66,17 @@ class AdminPortal extends Component {
               this.state.displayItem === 'create' &&
               <div>
                 <div>
-                  <input type="text" placeholder='Event Title' />
-                  <input type="date" placeholder='Event Date' />
+                  <input type="text" placeholder='Event Title' onChange={this.newTitle} />
+                  <input type="date" placeholder='Event Date' onChange={this.newDate} />
+                  <textarea id="textarea2" className="materialize-textarea" onChange={this.newText}></textarea>
                 </div>
 
                 <div className="row">
-                  <a onClick={this.cancel} class="waves-effect waves-light btn">Cancel</a>
-                  <a class="waves-effect waves-light btn">Create</a>
+                  <a onClick={this.cancel} className="waves-effect waves-light btn">Cancel</a>
+                  <a onClick={this.createNewEvent} className="waves-effect waves-light btn">Create</a>
 
                 </div>
               </div>
-
-
             }
 
           </div>
