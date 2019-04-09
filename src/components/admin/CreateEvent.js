bb021d1a2af6ec5ce986d6609ee2ce46449223fa
SMB <sethmichaelbrown@gmail.com>
2019-04-09 12:26:47 -0700

    Adding even components

---
+++ src/components/admin/CreateEvent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateEvent = (props) => {
+  return (
+    <div className="CreateEvent">
+
+    </div>
+  )
+}
+
+export default CreateEvent
---
+++ src/components/admin/EditEvent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditEvent = (props) => {
+  return (
+    <div className="EditEvent">
+
+    </div>
+  )
+}
+
+export default EditEvent
